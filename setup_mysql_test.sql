eb89af4f38e1f179f7cbcbd2caf19ff84aeb48e1
juliancastaeda <1277@holbertonschool.com>
2020-03-30 17:26:11 -0500

    load file task 4 create user and datebase test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT ALL PRIVILEGES ON hbnb_test_db TO hbnb_test@localhost;
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
