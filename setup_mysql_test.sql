c709ac0b57989c762be3cd1add7a62a84a1671e8
juliancastaeda <1277@holbertonschool.com>
2020-03-30 17:30:25 -0500

    upload file task 4 create user and datebase test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
 --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db TO hbnb_test@localhost;
