b11d2b51957c9588079ca89142a7608838ad1ced
jackeado <pabsanchez@gmail.com>
2020-04-01 23:08:11 +0000

    update some methods

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,18 @@
 import uuid
 import models
 from datetime import datetime
+from slqalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DataTime, nullable=False, default=datatime.utcnow())
+    updated_at = Column(DataTime, nullable=False, default=datatime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -26,10 +32,17 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+                if 'created_at' != kwargs:
+                    self.created_at = datatime.now()
+                if 'updated_at' != kwargs:
+                    self.updated_at = datatime.now()
+                if 'id' != kwargs:
+                    self.id = str(uuid.uuid4())
+                    
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+
 
     def __str__(self):
         """returns a string
@@ -48,6 +61,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -59,4 +73,6 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict:
+            del new_dict("_sa_instance_state")
         return my_dict
