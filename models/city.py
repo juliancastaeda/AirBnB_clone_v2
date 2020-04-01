958cbf72fb3c1112da735d27a2ec4813248c732a
juliancastaeda <1277@holbertonschool.com>
2020-04-01 16:23:54 -0500

    upload file

--- models/city.py
+++ models/city.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
 
 class City(BaseModel):
     """This is the class for City
@@ -9,5 +12,13 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+    elif os.getenv('HBNB_TYPE_STORAGE') == 'file':
+        state_id = ''
+        name = ''
