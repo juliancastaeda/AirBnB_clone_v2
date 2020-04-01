58d9f10a40b1d0ab2570a245f4ceb3ace8f80418
juliancastaeda <1277@holbertonschool.com>
2020-04-01 11:24:41 -0500

    upload file

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column,Integer,Sequence, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
-
-class User(BaseModel):
+class User(BaseModel, base):
     """This is the class for user
     Attributes:
         email: email address
@@ -11,9 +13,15 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = 'users'
-    
-    email = Column(String(128),unique=True)
-    password = Column(String(128))
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    if getenv('HBNB_TYPE_STORAGE') == DBStorage:
+        __tablename__ = 'users'
+        email = Column(String(128), unique=True, nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128)), nullable=True)
+
+    elif getenv('HBNB_TYPE_STORAGE') == FileStorage:
+        email = ""
+        password = ""
+        first_name = ""
+        lastname = ""
