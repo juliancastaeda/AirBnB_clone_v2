9c59431409c8584716e5439c0517a24570a1471f
juliancastaeda <1277@holbertonschool.com>
2020-04-01 15:53:13 -0500

    upload file

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column,Integer,Sequence, String
+from sqlalchemy import Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from sqlalchemy.orm import relationship
+import os
 
-class User(BaseModel, base):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -13,11 +14,15 @@ class User(BaseModel, base):
         first_name: first name
         last_name: last name
     """
-    if getenv('HBNB_TYPE_STORAGE') == DBStorage:
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), unique=True, nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         
-    
+    elif os.getenv('HBNB_TYPE_STORAGE') == 'File':
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
