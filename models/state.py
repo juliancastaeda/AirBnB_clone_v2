97ce318c9656466072953c57f27aa15350b0a220
jackeado <pabsanchez@gmail.com>
2020-04-02 22:40:03 +0000

    fix

--- models/state.py
+++ models/state.py
@@ -17,6 +17,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     
     if models.storage_t == 'db':
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state",
                               cascade="all, delete-orphan")
@@ -25,6 +26,6 @@ class State(BaseModel, Base):
     else:
         name = ''
 
-        def __init__(self, *args, **kwargs):
-            """initializes state"""
-            super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
