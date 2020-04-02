a645a79d0a409b8c7a4dcb2a6a6ef4688394d2b3
jackeado <pabsanchez@gmail.com>
2020-04-02 01:34:56 +0000

    update

--- models/city.py
+++ models/city.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 """This is the city class"""
+import models
+import sqlalchemy
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-import os
+from os import getenv
 
-class City(BaseModel):
+
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship("Place", backref="cities",
-                              cascade="all, delete-orphan")
+    if models.storage_t == 'db':
+        __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-    elif os.getenv('HBNB_TYPE_STORAGE') == 'file':
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
+
+    else:
         state_id = ''
         name = ''
