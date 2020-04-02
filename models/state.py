bed1e669ea43335bc8d2b79900b3fc78b14da856
jackeado <pabsanchez@gmail.com>
2020-04-02 01:35:13 +0000

    update

--- models/state.py
+++ models/state.py
@@ -3,9 +3,23 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "cities"
+
+    if models.storage_t == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    else:
+        name = ''
+
+        def __init__(self, *args, **kwargs):
+            """initializes state"""
+            super().__init__(*args, **kwargs)
