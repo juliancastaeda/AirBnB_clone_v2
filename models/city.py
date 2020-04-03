118b4108cb552ff0e067e01478ef82ddfb7a04e0
jackeado <pabsanchez@gmail.com>
2020-04-03 00:31:58 +0000

    fix

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os 
+import os 
 
 
 class City(BaseModel, Base):
--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os
+from os import environ
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
