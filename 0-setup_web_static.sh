84232876e8a0851cc4f7fd96f6c1d730f9744202
juliancastaeda <1277@holbertonschool.com>
2020-04-18 17:19:49 -0500

    load file task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-#script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo echo "This is a test" | sudo sed /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chmod -hR 700 ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+#!/usr/bin/env bash
+#
+sudo apt-get -y update && sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo -e "<html>\n <head>\n </head>Hello Holberton/n <body>hi/n</body>/n<html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '42i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
