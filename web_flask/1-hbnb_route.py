c687eb9171b31798b811ac0687cb1a3f10963248
juliancastaeda <1277@holbertonschool.com>
2020-04-22 15:55:30 -0500

    load file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/',  strict_slashes=False)
+def hbnb_route0():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb',  strict_slashes=False)
+def hbnb_route1():
+    return 'HBNB!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
