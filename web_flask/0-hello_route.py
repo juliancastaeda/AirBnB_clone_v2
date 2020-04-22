5a01937f86dc94e245114b6da55fbcb6e8bfb946
juliancastaeda <1277@holbertonschool.com>
2020-04-22 15:14:47 -0500

    load file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,12 +2,11 @@
 """
 """
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
 
 if __name__=="__main__":
-   app.run(debug=True)
+   app.run(host='0.0.0.0', port=5000)
