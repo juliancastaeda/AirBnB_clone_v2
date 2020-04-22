dfad7689116d3cfff042de59ba64f5b36dd352ca
juliancastaeda <1277@holbertonschool.com>
2020-04-22 17:15:20 -0500

    load file

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,6 +19,7 @@ def index_hbnb():
 def index_hbnb_C(text):
     return "C %s" % text.replace('_', ' ')
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def index_hbnb_python(text='is cool'):
